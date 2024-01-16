2a902a1444e742bd75a0db6d8ee86e4660fac2c6
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-16 12:45:16 +0100

    Add files via upload

---
+++ Promjena_recenice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Unesi neku recenicu: ");
+        string unos = Console.ReadLine();
+
+        string sVelika = unos.ToUpper();
+        string sMala = unos.ToLower();
+        string sPrvaTri = unos.Substring(0, 3);
+        string sZadnjihPet = unos.Substring(unos.Length - 5);
+        string s8_11 = unos.Substring(7, 4);
+
+        Console.WriteLine("sVelika: " + sVelika);
+        Console.WriteLine("sMala: " + sMala);
+        Console.WriteLine("sPrvaTri: " + sPrvaTri);
+        Console.WriteLine("sZadnjihPet: " + sZadnjihPet);
+        Console.WriteLine("s8_11: " + s8_11);
+
+        Console.ReadKey();
+    }
+}
